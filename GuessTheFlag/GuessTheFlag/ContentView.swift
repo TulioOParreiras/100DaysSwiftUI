181aaed89fa5739f71d165d9e52587fb8574838e
Tulio Parreiras <tulio@usemobile.xyz>
2019-10-18 15:10:25 -0300

    GuessTheFlag app finished (Day 22)

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,76 +10,71 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
-    @State private var showingAlert = false
-    
-    let count = 5
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var score = 0
     
     var body: some View {
-//        VStack {
-//            ForEach(0 ..< self.count) {_ in
-//                HStack {
-//                    ForEach(0 ..< self.count) { _ in
-//                        Text("1")
-//                    }
-//                }
-//            }
-//        }
-        
-//        ZStack {
-////            Color.red.frame(width: 200, height: 200)
-////            Color(red: 1, green: 0.8, blue: 0)
-////            Color.red.edgesIgnoringSafeArea(.all)
-//            Text("Your content")
-//            .background(Color.red)
-//        }
-        
-//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-//        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-        
-//        Button(action: {
-//            print("Button was tapped")
-//        }) {
-//            HStack(spacing: 10) {
-//                Image(systemName: "pencil")
-//                Text("Edit")
-//            }
-//            .foregroundColor(.purple)
-//        }
-//        Button("Tap me!") {
-//            print("Button was tapped")
-//        }
-        
-//        Button("Show Alert") {
-//            self.showingAlert = true
-//        }.alert(isPresented: $showingAlert) {
-//            Alert(title: Text("Hello SwiftUI"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
-//        }
         
         ZStack{
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
             
             VStack(spacing: 30){
                 VStack {
                     Text("Tap the flag of")
+                        .foregroundColor(.white)
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
                 }
                 
                 ForEach(0 ..< 3) { number in
                     Button(action: {
-                        // flad was tapped
+                        self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
                     }
                 }
+                
+                Text("Your score is \(self.score)")
+                    .foregroundColor(.white)
                 Spacer()
             }
         }
+        .alert(isPresented: $showingScore) { () -> Alert in
+            Alert(title: Text(scoreTitle), message: Text(scoreMessage), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            scoreMessage = ""
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+            scoreMessage = "That's the flag of \(countries[number])"
+            score -= 1
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        self.countries.shuffle()
+        self.correctAnswer = Int.random(in: 0 ... 2)
     }
     
 }
