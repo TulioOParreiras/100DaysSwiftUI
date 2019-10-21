aa0aef8bc47bea6a943e54dfe5b8862ae48464e8
Tulio Parreiras <tulio@usemobile.xyz>
2019-10-21 08:52:15 -0300

    ViewsAndModifiers project created (day 23)

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Usemobile on 21/10/19.
+//  Copyright © 2019 Usemobile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
