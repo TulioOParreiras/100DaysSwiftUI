5692c99337b854b08f4095abfb92c99fa0900030
Tulio Parreiras <tulio@usemobile.xyz>
2019-11-01 09:47:01 -0300

    iExpense project created

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Usemobile on 01/11/19.
+//  Copyright © 2019 Usemobile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
